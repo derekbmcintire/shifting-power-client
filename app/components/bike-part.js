10738149ce61e350cfbf79fd1974cab99cd55755
derekbmcintire <derekbmcintire@gmail.com>
2018-01-12 10:24:40 -0500

    Add hover actions for cranks and pedals
    - create and position <div> elements for cranks and pedals
    - add mouseEnter and mouseLeave actions
    - display part names in message element

--- app/components/bike-part.js
+++ app/components/bike-part.js
@@ -3,12 +3,23 @@ import Ember from 'ember';
 export default Ember.Component.extend({
   message: '',
   messages: {
-    backWheel: 'back wheel'
+    backWheel: 'back wheel',
+    frontWheel: 'front wheel',
+    cranks: 'cranks',
+    pedals: 'pedals'
   },
   actions: {
     backWheel() {
       this.set('message', this.get('messages.backWheel'))
-      // return this.sendAction('backWheel')
+    },
+    frontWheel() {
+      this.set('message', this.get('messages.frontWheel'))
+    },
+    cranks() {
+      this.set('message', this.get('messages.cranks'))
+    },
+    pedals() {
+      this.set('message', this.get('messages.pedals'))
     },
     resetMessage() {
       this.set('message', '')
--- app/routes/videos.js
+++ app/routes/videos.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  message: {
+  messages: {
     hello: 'hello'
   },
   actions: {
