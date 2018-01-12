9d31b6a8e52741fb8902b609e3f7e1fe1c52b490
derekbmcintire <derekbmcintire@gmail.com>
2018-01-12 09:15:05 -0500

    Create hover action on rear wheel
    - Add styling to bike-parts component.
    - Add mouseEnter and mouseLeave events for rearWheel div
    - In bike-part.js write object to store messages to display
    - In bike-part.js define action to display and hide message
      for hovering over back wheel.

--- app/components/bike-part.js
+++ app/components/bike-part.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  message: '',
+  messages: {
+    backWheel: 'back wheel'
+  },
+  actions: {
+    backWheel() {
+      this.set('message', this.get('messages.backWheel'))
+      // return this.sendAction('backWheel')
+    },
+    resetMessage() {
+      this.set('message', '')
+    }
+  }
 });
--- app/routes/videos.js
+++ app/routes/videos.js
@@ -1,5 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-
+  message: {
+    hello: 'hello'
+  },
+  actions: {
+    backWheel() {
+      console.log('backwheel')
+    }
+  }
 });
