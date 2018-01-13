b1564fdf75caa75916dee85cefbdc2c6279b9fe6
derekbmcintire <derekbmcintire@gmail.com>
2018-01-12 21:27:56 -0500

    Get videos and display in index page
    - begin work on show feature for videos

--- app/components/bike-video-container.js
+++ app/components/bike-video-container.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
     newVideo(url) {
       this.set('featuredUrl', url)
       console.log(this.get('featuredUrl'))
+    },
+    getVideo(id) {
+      return this.sendAction('getVideo', id)
     }
   }
 
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model () {
+    return this.get('store').findAll('video');
+  },
+  actions: {
+    getVideo(id) {
+      return this.get('store').findRecord('video', id)
+    }
+  }
 });
