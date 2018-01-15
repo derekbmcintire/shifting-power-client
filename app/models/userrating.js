5e7b3b17b641f92d3ecd5b50a986905e14dd27f4
derekbmcintire <derekbmcintire@gmail.com>
2018-01-14 19:32:22 -0500

    Create computed property for video model - sum
    - add sum computed property to video model
    - not successfully returning sum of ratings

--- app/models/userrating.js
+++ app/models/userrating.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  rating: DS.attr('integer')
+  rating: DS.attr('number')
 });
--- app/models/video.js
+++ app/models/video.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   title: DS.attr('string'),
   url: DS.attr('string'),
-  ratings: DS.hasMany('userrating')
+  ratings: DS.hasMany('userrating'),
+  sum: Ember.computed.sum('ratings')
 });
