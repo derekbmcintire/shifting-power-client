9508602fa35fccabcaf1100be0cb94ec2b90a2a8
derekbmcintire <derekbmcintire@gmail.com>
2018-01-15 13:39:22 -0500

    Show rate video if user is authenticated
    - only display the option to rate a video if current user is
      authenticated
    - inject auth service into featured-video component .js file
    - add conditional to featured-video template

--- app/components/featured-video.js
+++ app/components/featured-video.js
@@ -1,6 +1,9 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  auth: Ember.inject.service(),
+  user: Ember.computed.alias('auth.credentials.email'),
+  isAuthenticated: Ember.computed.alias('auth.isAuthenticated'),
   rating: null,
   updatedRating: Ember.computed('rating', function () {
     return this.getProperties('rating').rating
