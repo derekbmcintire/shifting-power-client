46bbab920d406740b9074109cfb888ef50dd47bc
derekbmcintire <derekbmcintire@gmail.com>
2018-01-16 13:13:10 -0500

    Almost update a rating
    - return filtered results
    - not successful in updating rating

--- app/routes/index.js
+++ app/routes/index.js
@@ -15,30 +15,39 @@ export default Ember.Route.extend({
       return this.transitionTo('videos', id)
   },
     newRating(rating) {
-      // check id's are as expected
-      // these are returning the correct id's
       console.log('user id ', this.getProperties('user').user)
       console.log('video id ', rating.video.id)
-
+      console.log('this rating is ', rating.rating)
       // send a GET request to get userratings
       // filtered by user_id and video_id
       this.get('store').query('userrating', {
+        // passes filter as a param to the index action
         filter: {
-          // this is not properly filtering and always returns
-          // all records
-          // tried passing entire user and video objects
-          // instead of id's but got errors
           user_id: this.getProperties('user').user,
           video_id: rating.video.id
         }
       }).then(function(result) {
         // when results are returned check to see what they are
-        // having trouble seeing what the result is
-        // tried result.userratings and got undefined
-        console.log('result length is ', result.get('length'))
+        // returning the correct length
+        console.log('first result id is ', result.get('firstObject').id)
         // if result exists log that user has already rated video
-        // i may need to check for length instead of simply existing
+
+        // find the first returned record by id
+        // something is going wrong here
         if (result) {
+          console.log(result.get('store'))
+          result.get('store').findRecord('userrating', (result.get('firstObject').id)).then(function(singleResult) {
+            // then set the 'rating' to the passed rating
+            // console.logging to see current and new ratings
+            console.log('result rating ', singleResult.get('rating'))
+            console.log('new rating is ', rating.rating)
+            // set the results rating to the new rating
+            singleResult.set('rating', rating.rating)
+            // save the result
+            singleResult.save()
+          }).then(function() {
+            console.log('updated successfully')
+          }).catch(console.error)
           console.log('you already rated this video')
         } else {
           // if nothing is returned, create a new record
