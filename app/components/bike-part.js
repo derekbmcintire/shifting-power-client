79a3f2651e98ca0284eb310ec0d35512d3073110
derekbmcintire <derekbmcintire@gmail.com>
2018-01-24 13:30:49 -0500

    Remove unnecessary actions
    - change show video actions to use getVideo and pass a tag number
    - remove old show video functions

--- app/components/bike-part.js
+++ app/components/bike-part.js
@@ -24,55 +24,18 @@ export default Ember.Component.extend({
       this.toggleProperty(name)
       this.set('message', msg);
     },
-    backWheelVideo() {
-      // this sends an action called getVideo up to the bike-video-container
-      // template and passes '11' as an argument.
-      // '11' represents the id of a tag which links this part to any number
-      // of related videos. In the future I would like each bike part
-      // to have multiple tags, so I will need to change getVideo to accept
-      // an array of tags and iterate through them, finding all the correct
-      // videos. In addition to that, the video that is most directly
-      // related to the part that was clicked on should be displayed
-      // as the first video.
-      this.sendAction('getVideo', '11');
-    },
+    // this sends an action called getVideo up to the bike-video-container
+    // template and passes '11' as an argument.
+    // '11' represents the id of a tag which links this part to any number
+    // of related videos. In the future I would like each bike part
+    // to have multiple tags, so I will need to change getVideo to accept
+    // an array of tags and iterate through them, finding all the correct
+    // videos. In addition to that, the video that is most directly
+    // related to the part that was clicked on should be displayed
+    // as the first video.
     getVideo(tagId) {
       this.sendAction('getVideo', tagId)
     },
-    frontWheelVideo() {
-      this.sendAction('getVideo', '11');
-    },
-    cassetteVideo() {
-      this.sendAction('getVideo', '8');
-    },
-    cranksVideo() {
-      this.sendAction('getVideo', '8');
-    },
-    chainVideo() {
-      this.sendAction('getVideo', '8');
-    },
-    frontDerVideo() {
-      this.sendAction('getVideo', '9');
-    },
-    rearDerVideo() {
-      this.sendAction('getVideo', '9');
-    },
-    hubVideo() {
-      this.sendAction('getVideo', '10');
-    },
-    brakesVideo() {
-      this.sendAction('getVideo', '12');
-    },
-    bbVideo() {
-      this.sendAction('getVideo', '10');
-    },
-    headsetVideo() {
-      this.sendAction('getVideo', '10');
-    },
-    shiftingVideo() {
-      this.sendAction('getVideo', '9');
-    },
-
     backWheel() {
       this.set('message', 'Back Wheel');
     },
